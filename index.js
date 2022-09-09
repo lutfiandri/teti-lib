84a77a6b26b9f193d7cdaf008fe544e79d19b86a
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-09-09 19:29:24 +0700

    feat(setup): mongodb connection

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 
+import { getBooksCollection, mongoClient, setDb } from './src/db/mongo.js';
 import booksRouter from './src/routes/booksRoute.js';
+import { exit } from 'process';
 
 const app = express();
 
 // TODO: create .env
 const PORT = 3000;
+
+mongoClient.connect((err) => {
+  if (err) {
+    console.log(`Can't connect to mongodb`);
+    exit(1);
+  }
+  console.log('Connected to mongodb');
+  const db = mongoClient.db('teti-lib');
+  setDb(db);
+});
 
 app.use(cors());
 
