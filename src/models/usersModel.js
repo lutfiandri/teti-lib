078f676879e5a95ba34982849ccc4551053e8ea2
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-09-24 09:23:27 +0700

    fix(user): set borrowedBookIds to not required

---
+++ src/models/usersModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    borrowedBookIds: {
+      type: [mongoose.Types.ObjectId],
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
