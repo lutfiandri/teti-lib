78f9096f74c4854296bf5889f11ad0059ed11207
junianN <rizky.j.n@mail.ugm.ac.id>
2022-09-24 09:10:20 +0700

    fix(users): fix usersModel

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -6,53 +6,18 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    // isbn: {
-    //   type: String,
-    //   required: true,
-    //   unique: true,
-    // },
-    // description: {
-    //   type: String,
-    //   required: true,
-    // },
-    // author: {
-    //   type: String,
-    //   required: true,
-    // },
-    // publisher: {
-    //   type: String,
-    //   required: true,
-    // },
-    // is_fiction: {
-    //   type: Boolean,
-    //   required: true,
-    // },
-    // num_of_books: {
-    //   type: String,
-    //   required: false,
-    // },
-    // genres: {
-    //   type: [String],
-    //   required: false,
-    // },
-    // num_of_pages: {
-    //   type: String,
-    //   required: false,
-    // },
-    // borrowers_id: {
-    //   type: [String],
-    //   required: false,
-    // },
-    // rating: {
-    //   type: Number,
-    //   required: false,
-    //   default: 0,
-    // },
-    // rating_count: {
-    //   type: Number,
-    //   required: false,
-    //   default: 0,
-    // },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    borrowedBooks: {
+      type: [mongoose.Types.ObjectId],
+      required: true,
+    },
   },
   { timestamps: true }
 );
