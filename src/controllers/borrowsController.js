fa8ab9d72c959693d2be0ba8e6afe91c47704748
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-09-29 10:59:54 +0700

    feat(borrow): filter and add by user

--- src/controllers/borrowsController.js
+++ src/controllers/borrowsController.js
@@ -1,66 +1,121 @@
 import mongoose from 'mongoose';
 import Borrow from '../models/borrowsModel.js';
 
+// Admin only
 export const findAll = async (req, res, next) => {
-    try {
-        const borrow = await Borrow.find({});
+  try {
+    const filter = {};
 
-        res.json(borrow);
+    // isReturned
+    if (req.query.returned === 'true') filter.isReturned = true;
+    if (req.query.returned === 'false') filter.isReturned = false;
 
-    } catch (err) {
-        next(err);
-    }
+    const borrow = await Borrow.find(filter);
+
+    res.json(borrow);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const findAllByUserId = async (req, res, next) => {
+  try {
+    let userId = req.user.isAdmin ? req.body.userId : req.user.id;
+
+    const filter = { userId: userId };
+
+    // isReturned
+    if (req.query.returned === 'true') filter.isReturned = true;
+    if (req.query.returned === 'false') filter.isReturned = false;
+
+    const borrows = await Borrow.find(filter);
+    res.json(borrows);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const findById = async (req, res, next) => {
+  try {
+    const id = mongoose.Types.ObjectId(req.params.id);
 
-        try {
-            const id = mongoose.Types.ObjectId(req.params.id);
+    const borrow = await Borrow.findById(id).exec();
 
-            const borrow = await Borrow.findById(id).exec();
-
-            if (borrow === null) {
-                next({
-                    message: `Borrowing session with id ${id} is not found`,
-                    stack: null,
-                    statusCode: 404,
-                });
-            }
-
-            res.json({
-                id: id,
-                borrow: borrow,
-            });
-        } catch (err) {
-            next(err);
-        }
-    };
-
-export const create = async (req, res, next) => {
-    try {
-        const borrow = new Borrow(req.body);
-        const result = await borrow.save();
-        res.status(201).json(result);
-    } catch (err) {
-        if (['CastError', 'ValidationError'].includes(err?.name)) {
-            next({
-                message: err.message,
-                stack: err.stack,
-                statusCode: 400,
-            });
-        }
-        next(err);
+    if (borrow === null) {
+      next({
+        message: `Borrowing session with id ${id} is not found`,
+        stack: null,
+        statusCode: 404,
+      });
     }
+
+    res.json({
+      id: id,
+      borrow: borrow,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
+export const create = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const borrow = new Borrow({
+      userId: mongoose.Types.ObjectId(userId),
+      bookId: req.body.bookId,
+      borrowedAt: new Date(),
+      isReturned: false,
+    });
+    const result = await borrow.save();
+    res.status(201).json(result);
+  } catch (err) {
+    if (['CastError', 'ValidationError'].includes(err?.name)) {
+      next({
+        message: err.message,
+        stack: err.stack,
+        statusCode: 400,
+      });
+    }
+    next(err);
+  }
+};
+
+export const updateReturn = async (req, res, next) => {
+  try {
+    const result = await Borrow.updateOne(
+      {
+        // look for this doc
+        userId: mongoose.Types.ObjectId(req.user.id),
+        bookId: mongoose.Types.ObjectId(req.body.bookId),
+        isReturned: false,
+      },
+      {
+        // and update the doc's value
+        isReturned: true,
+        returnedAt: new Date(),
+      }
+    );
+    res.status(200).json(result);
+  } catch (err) {
+    if (['CastError', 'ValidationError'].includes(err?.name)) {
+      next({
+        message: err.message,
+        stack: err.stack,
+        statusCode: 400,
+      });
+    }
+    next(err);
+  }
+};
 
 export const deleteById = async (req, res, next) => {
-    try {
-        const id = mongoose.Types.ObjectId(req.params.id);
+  try {
+    const id = mongoose.Types.ObjectId(req.params.id);
 
-        const response = await Borrow.deleteOne({ _id: id });
-        res.json({ response });
-    } catch (err) {
-        next(err);
-    }
+    const response = await Borrow.deleteOne({ _id: id });
+    res.json({ response });
+  } catch (err) {
+    next(err);
+  }
 };
--- src/routes/borrowsRoute.js
+++ src/routes/borrowsRoute.js
@@ -7,11 +7,17 @@ const router = express.Router();
 // Get all borrows
 router.get('/', auth.authenticate, controller.findAll);
 
+// Get all books that the user borrows
+router.get('/user', auth.authenticate, controller.findAllByUserId);
+
 // Get specific borrows by id
 router.get('/:id', auth.authenticate, controller.findById);
 
 // Borrow new book
 router.post('/', auth.authenticate, controller.create);
+
+// Return borrowed book
+router.put('/return', auth.authenticate, controller.updateReturn);
 
 // Delete specific borrow by id
 // router.delete('/:id', controller.deleteById);
